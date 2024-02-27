ecb5d480a4839a895caa26d262f5b77e01c580d5
SaifM <mohd.saiful@live.com>
2024-02-27 21:56:00 +0800

    feat: 🎸 Added remap to ctrld/ctrlu

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,4 +1,9 @@
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set('i', 'jj', '<Esc>', { noremap = true })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true }) 
+
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true }) 
