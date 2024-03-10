16007640e44bb6cf3d0926090a503ea9a12d80ed
SaifM <mohd.saiful@live.com>
2024-03-11 02:20:22 +0800

    feat: 🎸 Added remap for moving lines

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -7,3 +7,15 @@ vim.keymap.set('i', 'jj', '<Esc>', { noremap = true })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
 
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
+
+-- Normal mode mappings
+vim.api.nvim_set_keymap('n', '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true })
+
+-- Insert mode mappings
+vim.api.nvim_set_keymap('i', '<A-j>', '<Esc>:m .+1<CR>==gi', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<A-k>', '<Esc>:m .-2<CR>==gi', { noremap = true, silent = true })
+
+-- Visual mode mappings
+vim.api.nvim_set_keymap('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
